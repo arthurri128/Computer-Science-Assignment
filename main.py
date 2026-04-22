891bd3ecf2bf1b6f2775cfb0697f01d94fab242b
Riley Arthur <riley@Mac.lan>
2026-04-22 20:37:40 +0800

    Product Class Finished

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from location import Location
 from product import Product
 from warehouse import Warehouse
 
-print("Live Alex Live")
+print("")
--- product.py
+++ product.py
@@ -1,5 +1,20 @@
+# Product class
 class Product:
-    
-    
-    
-    pass
+    #Attributes that store the product information
+    def __init__(self, name, sku, price, quantity):
+        self.name = name        # Name of the product
+        self.sku = sku          # Identifier of product
+        self.price = price      # Price of product
+        self.quantity = quantity # Current level of stock
+
+    # Method that updates the quantity, including validation
+    def update_quantity(self, amount):
+        # This prevents a possible negative quantity being inputted
+        if self.quantity + amount >= 0:
+            self.quantity += amount
+        else:
+            print("Quantity cannot be negative, please choose a positive value")
+
+    # Displays All Product Details
+    def display(self):
+        print(f"Product Name: {self.name}, SKU: {self.sku}, Price: ${self.price}, Quantity: {self.quantity}")
