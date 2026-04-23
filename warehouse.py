26a9289d73f0326d9e1704b31981a5442314ec85
Riley Arthur <riley@Riley-3.local>
2026-04-23 12:23:39 +0800

    Warehouse Started

--- warehouse.py
+++ warehouse.py
@@ -1,2 +1,11 @@
 class Warehouse:
-    pass
+
+    # Privatising the attributes and setting them to self.__
+    def __init__(self, rows, cols, grid):
+        self.rows = rows
+        self.cols = cols
+        self.grid[rows][cols]
+    
+    # Adds location within the warehouse
+    def add_location(location):
+        self.grid[location.rows][location.cols] = location
