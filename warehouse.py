1d4b7745a44af663f82ba42f8d1b77a3dc2a377c
Riley Arthur <riley@Mac.lan>
2026-04-23 20:13:32 +0800

    Warehouse slightly finished, lots of errors hence commit being used

--- warehouse.py
+++ warehouse.py
@@ -1,11 +1,17 @@
 class Warehouse:
 
     # Privatising the attributes and setting them to self.__
-    def __init__(self, rows, cols, grid):
+    def __init__(self, rows, cols,):
         self.rows = rows
         self.cols = cols
-        self.grid[rows][cols]
+        self.grid = []
+        for i in range(rows):
+            row = []
+            for j in range(cols):
+                row.append(None)
+            self.grid.append(row)
+
     
     # Adds location within the warehouse
-    def add_location(location):
-        self.grid[location.rows][location.cols] = location
+    def add_location(self, location):
+        self.grid[location.row][location.col] = location
